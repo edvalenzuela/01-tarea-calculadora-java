02de3f1b42eb8d9ceb7236b2aa035e7a1a3fb271
Eduardo Valenzuela <eduardo.valenzuela@nobilis.cl>
2022-10-02 19:22:12 -0300

    ✨ agregando excepciones en el menú

--- calculadora/Calculator.java
+++ calculadora/Calculator.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class Calculator {
 
-	static int optionMenu = 0;
-	static int optionMenu2 = 0;
+	static int optionMenu, optionMenu2;
+	static int num1, num2;
 	static String messagePath = "=== Resultado === \n";
 
 	public static int getSum(int num1, int num2){
@@ -37,50 +37,74 @@ public class Calculator {
 
 		return dividiendo;
 	}
-	
-	public static int messageNumbers1() throws IOException{
-		System.out.println("Ingrese el 1er número");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num1 = Integer.parseInt(br.readLine());
 
+	public static void getMessageError(String message, Exception e){
+		System.out.println(message + e.getMessage());
+	}
+	
+	public static int getNumberOne() throws IOException{
+		try {
+			System.out.println("Ingrese el 1er número");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			num1 = Integer.parseInt(br.readLine());
+		} catch (NumberFormatException e) {
+			getMessageError("Ingrese un valor númerico !!! ", e);
+		}catch (ArithmeticException e){
+			getMessageError("Error en tiempo de ejecución !!! " , e);
+		}
 		return num1;
 	}
 
-	public static int messageNumbers2() throws IOException{
-		System.out.println("Ingrese el 2do número");
-		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
-		int num2 = Integer.parseInt(br2.readLine());
-
+	public static int getNumberTwo() throws IOException{
+		try {
+			System.out.println("Ingrese el 2do número");
+			BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+			num2 = Integer.parseInt(br2.readLine());
+		} catch (NumberFormatException e) {
+			getMessageError("Ingrese un valor númerico !!! ", e);
+		}	catch (ArithmeticException e){
+			getMessageError("Error en tiempo de ejecución !!! " , e);
+		}
 		return num2;
 	}
 
 	public static void messageCalculator() throws IOException{
-		System.out.println("");
-		System.out.println("===== Calculator =====");
-		System.out.println("=== Ingrese el número del menú === ");
-		System.out.println("");
-		System.out.println("1. Sumar");
-		System.out.println("2. Restar");
-		System.out.println("3. Multiplicar");
-		System.out.println("4. Dividir");
-		System.out.println("5. Apagar calculadora");
-		System.out.println("");
+		try {
+			System.out.println("");
+			System.out.println("===== Calculator =====");
+			System.out.println("=== Ingrese el número del menú === ");
+			System.out.println("");
+			System.out.println("1. Sumar");
+			System.out.println("2. Restar");
+			System.out.println("3. Multiplicar");
+			System.out.println("4. Dividir");
+			System.out.println("5. Apagar calculadora");
+			System.out.println("");
 
-		BufferedReader op = new BufferedReader(new InputStreamReader(System.in));
-		optionMenu = Integer.parseInt(op.readLine());
+			BufferedReader op = new BufferedReader(new InputStreamReader(System.in));
+			optionMenu = Integer.parseInt(op.readLine());
+
+		} catch (NumberFormatException e) {
+			getMessageError("Ingrese un valor númerico !!! ", e);
+		}
 	}
 
 	public static void messageFinish() throws IOException{
-		System.out.println("");
-		System.out.println("===== Seguro que quieres apagar la calculadora? =====");
-		System.out.println("=== Ingrese el número del menú === ");
-		System.out.println("");
-		System.out.println("1. Si");
-		System.out.println("2. No");
-		System.out.println("");
+		try {
+			System.out.println("");
+			System.out.println("===== ¿ Seguro que quieres apagar la calculadora ? =====");
+			System.out.println("=== Ingrese el número del menú === ");
+			System.out.println("");
+			System.out.println("1. Si");
+			System.out.println("2. No");
+			System.out.println("");
+	
+			BufferedReader op = new BufferedReader(new InputStreamReader(System.in));
+			optionMenu2 = Integer.parseInt(op.readLine());
 
-		BufferedReader op = new BufferedReader(new InputStreamReader(System.in));
-		optionMenu2 = Integer.parseInt(op.readLine());
+		} catch (NumberFormatException e) {
+			getMessageError("Ingrese un valor númerico !!! ", e);
+		}
 	}
 
 	public static void getOption(int option) throws IOException{
@@ -97,19 +121,19 @@ public class Calculator {
 
 			switch (optionMenu) {
 				case 1:
-					getSum(messageNumbers1(), messageNumbers2());
+					getSum(getNumberOne(), getNumberTwo());
 				break;
 
 				case 2:
-					getRest(messageNumbers1(), messageNumbers2());
+					getRest(getNumberOne(), getNumberTwo());
 				break;
 
 				case 3:
-					getMulti(messageNumbers1(), messageNumbers2());
+					getMulti(getNumberOne(), getNumberTwo());
 				break;
 
 				case 4:
-					getDivi(messageNumbers1(), messageNumbers2());
+					getDivi(getNumberOne(), getNumberTwo());
 				break;
 
 				case 5:
